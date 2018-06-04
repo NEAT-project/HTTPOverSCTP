9cc5581153abc7cc2ea6d9216470d6a55e430106
Felix Weinrank <weinrank@bsd1.fh-muenster.de>
2018-06-04 14:55:59 +0200

    add 'test.sh' file

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#export HTTP_TRANSPORT_PROTOCOL=TCP
+#export HTTP_USE_PIPELINING=YES
+#export HTTP_DEBUG=LOG_DBG
+#export HTTP_SCTP_MAX_STREAMS=2
+#while true; do
+	./phttpget interop.nplab.de files/1G
+#done
